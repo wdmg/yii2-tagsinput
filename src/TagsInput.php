68d8e81bad6965bc4d846ea98fbc58e19083773b
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2019-11-26 03:39:30 +0200

    Added widget plugin and assets

---
+++ src/TagsInput.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace wdmg\widgets;
+
+/**
+ * Yii2 TagsInput
+ *
+ * @category        Widgets
+ * @version         1.0.1
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @link            https://github.com/wdmg/yii2-tagsinput
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ *
+ */
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\widgets\InputWidget;
+use yii\base\InvalidConfigException;
+
+class TagsInput extends InputWidget
+{
+
+    public $items = [];
+
+    public $options = [
+        'class' => 'form-control'
+    ];
+
+    public $pluginOptions = [
+        'delimiter' => ',',
+        'inputClass' => '.tagsinput',
+        'labelClass' => '.label .label-info',
+        'autocomplete' => false,
+        'minInput' => 2,
+        'maxTags' => 10
+    ];
+
+    private $tagsinputId = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        // Build select
+        $input = Html::activeInput('text', $this->model, $this->attribute, $this->items, $this->options);
+
+        // Get select id
+        $this->tagsinputId = $this->options['id'];
+        $this->pluginOptions['id'] = $this->tagsinputId;
+
+        // Register assets
+        $this->registerAssets();
+
+        echo $input;
+    }
+
+    /**
+     * Register required assets for the widgets
+     */
+    public function registerAssets()
+    {
+        $js = [];
+        $view = $this->getView();
+
+        // Register tagsinput assets to view
+        TagsInputAssets::register($view);
+
+        // Parse plugin options and insert inline
+        $pluginOptions = !empty($this->pluginOptions) ? Json::encode($this->pluginOptions) : '';
+        $js[] = "; jQuery('#" . $this->tagsinputId . "').tagsinput($pluginOptions);";
+
+        // Register tagsinput component initial script
+        $view->registerJs(implode("\n", $js));
+
+    }
+
+}
---
+++ src/TagsInputAssets.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace wdmg\widgets;
+
+/**
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ */
+
+use yii\web\AssetBundle;
+
+class TagsInputAssets extends AssetBundle
+{
+
+    public $sourcePath = '@bower/bootstrap-tagsinput-plugin/src';
+
+    public function init()
+    {
+        parent::init();
+        $this->css = YII_DEBUG ? ['css/tagsinput.css'] : ['css/tagsinput.min.css'];
+        $this->js = YII_DEBUG ? ['js/tagsinput.js'] : ['js/tagsinput.min.js'];
+        $this->depends = [\yii\web\JqueryAsset::className()];
+    }
+
+}
